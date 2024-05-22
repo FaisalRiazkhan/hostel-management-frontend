032bf12ad296d7b6ea1d9482181bc5a80e80225d
Faisal Riaz <faisal.riaz@innovotechnologies.com>
2024-05-22 11:28:26 +0500

    error resolved for setting default base URL

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
-// store / auth.js
-
 const state = {
   user: JSON.parse(localStorage.getItem('user')) || null,
   isAuthenticated: localStorage.getItem('authenticated') === 'true',
@@ -27,39 +25,36 @@ const mutations = {
   setAuthentication(state, isAuthenticated) {
     state.isAuthenticated = isAuthenticated
     if (isAuthenticated) {
-      localStorage.setItem('authenticated', 'true') // Store flag in localStorage upon successful login
+      localStorage.setItem('authenticated', 'true') 
     } else {
-      localStorage.setItem('authenticated', 'false') // Remove flag from localStorage upon logout
+      localStorage.setItem('authenticated', 'false')
     }
   },
   logout(state) {
-    // console.log('Logging out...'); // Log the logout action
     state.user = null
     state.token = null
-    // state.authenticated = false;
     // Clear any other authentication-related state variables
     localStorage.removeItem('authenticated')
     localStorage.removeItem('token')
     localStorage.removeItem('user')
-    console.log('Logout completed.') // Log completion of logout
+    console.log('Logout completed.') 
   }
 }
 
 const actions = {
   async login({ commit }, { email, password }) {
     try {
-        const response = await axios.post('login', { email, password });
-        const { token } = response.data;
-        commit('setToken', token);
-        // Fetch user details using the obtained token
-        const userResponse = await axios.get('logged_user', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+      const response = await axios.post('login', { email, password })
+      const { token } = response.data
+      commit('setToken', token)
+      // Fetch user details using the obtained token
+      const userResponse = await axios.get('logged_user', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
 
       const user = userResponse.data
-      // console.log('user data on login:', user);
       commit('setUser', user)
       commit('setAuthentication', true)
       return true
@@ -76,7 +71,7 @@ const actions = {
         throw new Error('Authentication token is not available')
       }
       await axios.post(
-        'http://127.0.0.1:8000/api/logout',
+        'logout',
         {},
         {
           headers: {
@@ -111,7 +106,7 @@ const actions = {
       console.log('fetched user data:', user)
     } catch (error) {
       console.error('Failed to fetch user', error)
-      commit('logout') 
+      commit('logout')
     }
   }
 }
@@ -127,7 +122,7 @@ const getters = {
 }
 
 export default {
-  namespaced: true, // Ensure that this module is namespaced
+  namespaced: true, 
   state,
   mutations,
   actions,
